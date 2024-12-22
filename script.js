849816767694c4ededb5cf0a1d88e50530fc550b
Hadiya <hadiyashaikh2006@gmail.com>
2024-12-22 11:37:37 +0530

    animated portfolio showcasing my skills.

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed=new Typed(".multiple-text",{
+    String:["FrontEnd Developer", "Backend Developer","Graphic Designer"],
+    tyoeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
